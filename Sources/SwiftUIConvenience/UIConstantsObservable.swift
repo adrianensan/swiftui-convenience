30d26f4bb46deb69e4a53be3b46f0838ba6931b5
Adrian Ensan <adrianensan@gmail.com>
2021-08-11 18:14:27 -0400

    Make some properties public

--- Sources/SwiftUIConvenience/UIConstantsObservable.swift
+++ Sources/SwiftUIConvenience/UIConstantsObservable.swift
@@ -1,21 +1,21 @@
 import SwiftUI
 import SwiftConvenience
 
-class UIConstantsObservable: ObservableObject {
+public class UIConstantsObservable: ObservableObject {
   
-  static var main: UIConstantsObservable = UIConstantsObservable()
+  public static var main: UIConstantsObservable = UIConstantsObservable()
  
   #if os(macOS)
-  @Published var screenSize: CGSize = NSApplication.shared.mainWindow?.frame.size ?? CGSize(width: 200, height: 400)
-  @Published var safeAreaInsets: EdgeInsets = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+  @Published public var screenSize: CGSize = NSApplication.shared.mainWindow?.frame.size ?? CGSize(width: 200, height: 400)
+  @Published public var safeAreaInsets: EdgeInsets = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
   #else
-  @Published var screenSize: CGSize = UIScreen.main.bounds.size
-  @Published var safeAreaInsets: EdgeInsets = (UIApplication.shared.windows.first?.safeAreaInsets ?? .zero) =& {
+  @Published public var screenSize: CGSize = UIScreen.main.bounds.size
+  @Published public var safeAreaInsets: EdgeInsets = (UIApplication.shared.windows.first?.safeAreaInsets ?? .zero) =& {
     EdgeInsets(top: $0.top, leading: $0.left, bottom: $0.bottom, trailing: $0.right)
   }
   #endif
   
-  var horizontalMargin: CGFloat { 6 + max(0, min(24, (screenSize.width - 320) / 3)) }
+  public var horizontalMargin: CGFloat { 6 + max(0, min(24, (screenSize.width - 320) / 3)) }
   
   func update(size: CGSize, safeArea: EdgeInsets) {
     //    if Device.current.screenCornerRadius == 0 {
