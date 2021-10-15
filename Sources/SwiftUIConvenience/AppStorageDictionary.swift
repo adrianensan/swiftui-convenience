8f239352bd382c31edf9fbda3924d37940af298b
Adrian Ensan <adrianensan@gmail.com>
2021-10-14 20:05:13 -0400

    Make codable extensions public

--- Sources/SwiftUIConvenience/AppStorageDictionary.swift
+++ Sources/SwiftUIConvenience/AppStorageDictionary.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
-extension Encodable {
+public extension Encodable {
   var jsonEncoded: Data? {
     try? JSONEncoder().encode(self)
   }
 }
 
-extension Data {
+public extension Data {
   func decoded<DecodedType: Decodable>() -> DecodedType? {
     try? JSONDecoder().decode(DecodedType.self, from: self)
   }
@@ -27,6 +27,27 @@ extension Dictionary: RawRepresentable where Key: Codable, Value: Codable {
           let result = String(data: data, encoding: .utf8)
     else {
       return "[]"
+    }
+    return result
+  }
+}
+
+public protocol AppStorageCodable: Codable {}
+public extension AppStorageCodable {
+  init?(rawValue: String) {
+    guard let data = rawValue.data(using: .utf8),
+          let result = try? JSONDecoder().decode(Self.self, from: data)
+    else {
+      return nil
+    }
+    self = result
+  }
+  
+  var rawValue: String {
+    guard let data = try? JSONEncoder().encode(self),
+          let result = String(data: data, encoding: .utf8)
+    else {
+      return ""
     }
     return result
   }
