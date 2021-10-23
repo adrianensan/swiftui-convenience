7bd256b0c26473015ce6c07fa5f22be86ec032c1
Adrian Ensan <adrianensan@gmail.com>
2021-10-23 12:41:59 -0400

    Add image rendeering

--- Package.swift
+++ Package.swift
@@ -1,6 +1,21 @@
 // swift-tools-version:5.5
 import PackageDescription
 
+let useLocal = false
+let dependencies: [Package.Dependency]
+if useLocal {
+  dependencies = [
+    .package(name: "SwiftConvenience",
+             path: "~/Repos/swift-packages/swift-convenience")
+  ]
+} else {
+  dependencies = [
+    .package(name: "SwiftConvenience",
+             url: "https://github.com/hello-apps/swift-convenience",
+             branch: "main")
+  ]
+}
+
 let package = Package(
     name: "SwiftUIConvenience",
     platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14)],
@@ -9,9 +24,13 @@ let package = Package(
         name: "SwiftUIConvenience",
         targets: ["SwiftUIConvenience"]),
     ],
+    dependencies: dependencies,
     targets: [
         .target(
-            name: "SwiftUIConvenience"),
+          name: "SwiftUIConvenience",
+          dependencies: [
+            .byNameItem(name: "SwiftConvenience", condition: nil)
+          ]),
         .testTarget(
             name: "SwiftUIConvenienceTests",
             dependencies: ["SwiftUIConvenience"]),
---
+++ Sources/SwiftUIConvenience/ViewImageRenderer.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+import SwiftConvenience
+
+enum ImageRenderer {
+  #if os(iOS)
+  static func render<Content: View>(view: Content, size: CGSize) -> UIImage? {
+    let controller = UIHostingController(rootView: view)
+    guard let view = controller.view else { return nil }
+    
+    let targetSize = CGSize(width: cellSize, height: cellSize)
+    view.frame.size = targetSize
+    view.backgroundColor = .clear
+    
+    let renderer = UIGraphicsImageRenderer(size: targetSize)
+    
+    var failed = false
+    let image = renderer.image { _ in
+      guard view.drawHierarchy(in: view.frame, afterScreenUpdates: true) else {
+        return failed = true
+      }
+    }
+    return failed ? nil : image
+  }
+  #elseif os(macOS)
+  static func render<Content: View>(view content: Content, size: CGSize) -> NSImage? {
+    let screenSale = NSScreen.main!.backingScaleFactor
+    
+    let view = NSHostingView(rootView: content)
+    view.frame = CGRect(origin: .zero, size: size / screenSale)
+    
+    let targetSize = NSSize(width: size.width / screenSale, height: size.height / screenSale)
+    guard let imageRepresentation = view.bitmapImageRepForCachingDisplay(in: view.frame),
+          let newImage = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(size.width), pixelsHigh: Int(size.height), bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: .deviceRGB, bytesPerRow: 0, bitsPerPixel: 0)
+    else { return nil }
+    
+    view.cacheDisplay(in: view.frame, to: imageRepresentation)
+    newImage.size = targetSize
+    NSGraphicsContext.saveGraphicsState()
+    NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: newImage)
+    NSGraphicsContext.current?.imageInterpolation = .high
+    imageRepresentation.draw(in: NSRect(origin: .zero, size: targetSize))
+    NSGraphicsContext.restoreGraphicsState()
+    
+    if let imageData = newImage.representation(using: .png, properties: [.compressionFactor: 0.9]) {
+      return NSImage(data: imageData)
+    } else {
+      return nil
+    }
+  }
+  #endif
+}
